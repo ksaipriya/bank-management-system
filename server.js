ad4a51e3692128f99e93a35891c41d7f554bd518
KAMINWAR SAI PRIYA <k.saipriya08@gmail.com>
2021-12-24 11:24:22 +0530

    changed

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const middlewares = jsonServer.defaults({
 
 const port = process.env.PORT || 8000;
  server.use(middlewares);
+ server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+  }))
  server.use(router);
  server.listen(port,()=>{
      console.log(`Server is running on ${port}`);
